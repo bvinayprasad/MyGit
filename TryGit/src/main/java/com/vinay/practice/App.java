9b3d539ef7f7514d780f0c93437099314786694b
bvinayprasad <vinay.prasad@gmail.com>
2016-07-24 00:55:48 +0530

    My first commit

---
+++ TryGit/src/main/java/com/vinay/practice/App.java
@@ -0,0 +1,13 @@
+package com.vinay.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
